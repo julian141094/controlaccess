5b8723a5f0eec27a5593a18e94b3f60e12ac98a3
julian141094 <julian.arkangel@gmail.com>
2019-02-21 23:34:49 -0400

    creacion de modelo para el control de entrada y salida

--- employer/models.py
+++ employer/models.py
@@ -18,7 +18,7 @@ class UserData(models.Model):
     address = models.CharField(verbose_name="Direccion",max_length=128)
     phone = models.CharField(verbose_name="Celular",max_length=12)
     license = models.BooleanField(verbose_name="Carnet")
-    active = models.BooleanField(verbose_name="Activo")
+    active = models.BooleanField(verbose_name="Activo", default=True)
 
 class ExtraUserData(models.Model):
     
